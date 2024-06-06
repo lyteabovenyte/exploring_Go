b15e27e412fb2ccb67fd30e91df1e4d4b0a96c94
lyteatnyte <lyteatnyte@lyteatnyte.local>
2024-06-06 16:42:11 +0330

    implementing a custom URLValue flag to parse a url and tear down the url's scheme, host and path. using flag.Value interface

---
+++ 18/custom_flag_to_parse_url.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"reflect"
+)
+
+// defining a custom flag
+type URLValue struct {
+	URL *url.URL
+}
+
+// by defining a custom flag, we should implement flag.Value methods
+// to satisfy it's interface
+// there are two methods on type Value interface for the flag
+// 1. String()  2.Set(s string)
+func (v URLValue) String() string {
+	if v.URL != nil {
+		return v.URL.String()
+	}
+	return ""
+}
+
+// by defining a Set() method on a type, we can read in any custom values
+func (v URLValue) Set(s string) error {
+	if u, err := url.Parse(s); err != nil {
+		return err
+	} else {
+		*v.URL = *u
+	}
+	return nil
+}
+
+var u = &url.URL{}
+
+func init() {
+	flag.Var(&URLValue{u}, "url", "URL to parse")
+}
+
+func main() {
+	flag.Parse()
+	if reflect.ValueOf(*u).IsZero() {
+		panic("did you pass the url?")
+	}
+	fmt.Printf("{scheme: %q, host: %q, path: %q}", u.Scheme, u.Host, u.Path)
+}
